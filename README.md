fa134c401d9b523f50959fcf0385189d918b102e
mc <machuang2264@163.com>
2021-07-15 11:37:09 +0800

    注释

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-plugins
+# plugins
+## 我们来搞个插件
+ - Config 配置文件读取，结合自己的项目容器使用
--- src/Config/Config.php
+++ src/Config/Config.php
@@ -14,6 +14,11 @@ class Config
         return self::$_instance;
     }
 
+    /**
+     * @desc 设置配置文件路径
+     * @param $sConfigPath string 绝对路径
+     * @return $this
+     * */
     public function parser($sConfigPath)
     {
         $sConfigPath = rtrim($sConfigPath, DIRECTORY_SEPARATOR);
@@ -35,9 +40,17 @@ class Config
         return $this;
     }
 
-    public function get($sKey = null)
+    /**
+     * @desc 获取配置
+     * @param $sConfigKey string
+     * @return mixed
+     * */
+    public function get($sConfigKey = null)
     {
-        $oKeys = explode('.', $sKey);
+        if($sConfigKey && !is_string($sConfigKey)){
+            throw new \Exception("param not a string", -1);
+        }
+        $oKeys = $sConfigKey ? explode('.', $sConfigKey) : [];
         $val = $oConfig = self::$config;
         foreach($oKeys as $item){
             if(!isset($oConfig[$item])){
